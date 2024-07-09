b79454e2389eef895652544c5764483c78165e3b
nagato <87745335+nagato300@users.noreply.github.com>
2024-07-10 04:09:06 +1000

    Complete 06 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const divByFour = year % 4 === 0;
+    const divByHundred = year % 100 === 0;
+    const divByFourHundred = year % 400 === 0;
+    if (divByFour) {
+        if (divByHundred && !divByFourHundred) {
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
